8cc82011a8625bed0fe331f0c1b44b404c2c9dae
mohammedjavadkt <javadkt@gmail.com>
2023-10-22 23:54:44 +0530

    Initial Commit

---
+++ src/main/java/com/example/jsf/utils/JwtUtil.java
@@ -0,0 +1,31 @@
+package com.example.jsf.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+    private static final String SECRET_KEY = "U3dEE12FsdB2+4G";
+    private static final long EXPIRATION_TIME = 3600000; // 1 hour
+
+    public static String generateToken(String username) {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + EXPIRATION_TIME);
+
+        return Jwts.builder()
+            .setSubject(username)
+            .setIssuedAt(now)
+            .setExpiration(expiration)
+            .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+            .compact();
+    }
+
+    public static String validateToken(String token) {
+        try {
+            return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
